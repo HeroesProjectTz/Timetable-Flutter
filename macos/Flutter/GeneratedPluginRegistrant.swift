8a2eb3caa2d75e00182def562f472e91e59103b9
luqmantuke <lsuleiman2002@gmail.com>
2023-02-13 18:06:35 +0300

    upgraded googlefonts package version which was causing the app to break

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,7 @@ import facebook_auth_desktop
 import firebase_auth
 import firebase_core
 import flutter_secure_storage_macos
-import path_provider_macos
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
